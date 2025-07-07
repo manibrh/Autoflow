66c0d295d190cafc8d91768ed61602d4d081e394
manibrh <manikandan.braahmam@gmail.com>
2025-07-07 19:41:05 +0530

    Update tep_preprocess.py

--- tep_preprocess.py
+++ tep_preprocess.py
@@ -33,28 +33,41 @@ def write_xliff(data, input_file, output_file, src_lang='en', tgt_lang='fr', ver
             'original': os.path.basename(input_file)
         })
         body = ET.SubElement(file_tag, 'body')
+
         for i, (key, value) in enumerate(data.items(), start=1):
             tu = ET.SubElement(body, 'trans-unit', {'id': str(i), 'resname': key})
-            ET.SubElement(tu, 'source').text = value
+
+            # Raw-preserve <source>
+            source = ET.SubElement(tu, 'source')
+            source.text = None
+            tu.remove(source)
+            tu.append(ET.fromstring(f"<source>{value}</source>"))
+
             ET.SubElement(tu, 'target').text = ''
+
         tree = ET.ElementTree(xliff)
 
     elif version == '2.0':
         ns = "urn:oasis:names:tc:xliff:document:2.0"
         ET.register_namespace('', ns)
-        xliff = ET.Element('{%s}xliff' % ns, {
+        xliff = ET.Element(f'{{{ns}}}xliff', {
             'version': '2.0',
             'srcLang': src_lang,
             'trgLang': tgt_lang
         })
-        file_tag = ET.SubElement(xliff, '{%s}file' % ns, {
+        file_tag = ET.SubElement(xliff, f'{{{ns}}}file', {
             'id': os.path.basename(input_file)
         })
+
         for i, (key, value) in enumerate(data.items(), start=1):
-            unit = ET.SubElement(file_tag, '{%s}unit' % ns, {'id': str(i)})
-            segment = ET.SubElement(unit, '{%s}segment' % ns)
-            ET.SubElement(segment, '{%s}source' % ns).text = value
-            ET.SubElement(segment, '{%s}target' % ns).text = ''
+            unit = ET.SubElement(file_tag, f'{{{ns}}}unit', {'id': str(i)})
+            segment = ET.SubElement(unit, f'{{{ns}}}segment')
+
+            src = ET.SubElement(segment, f'{{{ns}}}source')
+            src.text = value
+
+            ET.SubElement(segment, f'{{{ns}}}target').text = ''
+
         tree = ET.ElementTree(xliff)
 
     else:
